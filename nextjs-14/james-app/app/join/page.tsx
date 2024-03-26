b0be58c628af1add4e353f712f83267475f624b2
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-03-26 18:22:18 +0900

    240326 @

--- nextjs-14/james-app/app/join/page.tsx
+++ nextjs-14/james-app/app/join/page.tsx
@@ -8,93 +8,89 @@ const SERVER = 'http://localhost:8080'
 
 export default function join() {
 
-    const [inputs, setInputs] = useState({
-        username : "",
-        password : "",
-        checkPassword : "",
-        name : "",
-        phone : "",
-        job : "",
-        height : "",
-        weight : ""
-    })
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+    checkPassword: "",
+    name: "",
+    phone: "",
+    job: "",
+    height: "",
+    weight: ""
+  })
 
-    const {username, password, checkPassword, name, phone, job, height, weight} = inputs;
-    const handleChange = (e:any) => {
-        const {value, name} = e.target;
-        setInputs(
-            {...inputs,
-        [username] : value,
-        [password] : value,
-        [checkPassword] : value,
-        [name] : value,
-        [phone] : value,
-        [job] : value,
-        [height] : value,
-        [weight] : value
-        })
-    }
-    
-    const router = useRouter();
+  const { username, password, checkPassword, name, phone, job, height, weight } = inputs;
+  const handleChange = (e: any) => {
+    const { value, name } = e.target;
+    setInputs(
+      {
+        ...inputs,
+        [name]: value
+      })
+  }
 
-    const handleSubmit = () => {alert("request가 가져가는 입력 값" + username + password + checkPassword + 
-    name + phone + job + height + weight)
+  const router = useRouter();
+
+  const handleSubmit = () => {
+    alert("request가 가져가는 입력 값" + username + password + checkPassword +
+      name + phone + job + height + weight)
     const url = `${SERVER}/api/users`
-    const data = {username, password, checkPassword, name, phone, job, height, weight} // data = requestbody
+    const data = { username, password, checkPassword, name, phone, job, height, weight } // data = requestbody
     const config = {
-      headers:{
+      headers: {
         "Cache-Control": "no-cache",
         "Content-Type": "application/json",
-         Authorization: `Bearer blah ~` ,
+        Authorization: `Bearer blah ~`,
         "Access-Control-Allow-Origin": "*",
-    } 
+      }
     }
-    axios.post(url,data,config) 
-    .then(res=>{
-      alert("response가 가져온 ID : " + JSON.stringify(res.data)) // response.responsebody = res.data = hashmap
-      router.push("/login")
-    })}
-    
+    axios.post(url, data, config)
+      .then(res => {
+        alert("response가 가져온 ID : " + JSON.stringify(res.data)) // response.responsebody = res.data = hashmap
+        router.push("/login")
+      })
+  }
 
-    return(<>
-        <div className="container">
-        <h1>Sign Up</h1>
-        <p>Please fill in this form to create an account.</p>
-        <hr/>
-        <label htmlFor="username"><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="username" onChange={handleChange} value={username}required/><br />
 
-        <label htmlFor="password"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="password" onChange={handleChange} value={password}required/><br />
+  return (<>
+    <div className="container">
+      <h1>Sign Up</h1>
+      <p>Please fill in this form to create an account.</p>
+      <hr />
+      <label htmlFor="username"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="username" onChange={handleChange} value={username} required /><br />
 
-        <label htmlFor="checkPassword"><b>Repeat Password</b></label>
-        <input type="password" placeholder="Repeat Password" name="checkPassword" onChange={handleChange} value={checkPassword}required/><br />
+      <label htmlFor="password"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="password" onChange={handleChange} value={password} required /><br />
 
-        <label htmlFor="name"><b>Name</b></label>
-        <input type="text" placeholder="Enter Name" name="name" onChange={handleChange} value={name}required/><br />
+      <label htmlFor="checkPassword"><b>Repeat Password</b></label>
+      <input type="password" placeholder="Repeat Password" name="checkPassword" onChange={handleChange} value={checkPassword} required /><br />
 
-        <label htmlFor="phone"><b>Phone Number</b></label>
-        <input type="text" placeholder="Enter Phone Number" name="phone" onChange={handleChange} value={phone}required/><br />
+      <label htmlFor="name"><b>Name</b></label>
+      <input type="text" placeholder="Enter Name" name="name" onChange={handleChange} value={name} required /><br />
 
-        <label htmlFor="job"><b>Job</b></label>
-        <input type="text" placeholder="Enter Job" name="job" onChange={handleChange} value={job}required/><br />
+      <label htmlFor="phone"><b>Phone Number</b></label>
+      <input type="text" placeholder="Enter Phone Number" name="phone" onChange={handleChange} value={phone} required /><br />
 
-        <label htmlFor="height"><b>Height</b></label>
-        <input type="text" placeholder="Enter Height" name="height" onChange={handleChange} value={height}required/><br />
+      <label htmlFor="job"><b>Job</b></label>
+      <input type="text" placeholder="Enter Job" name="job" onChange={handleChange} value={job} required /><br />
 
-        <label htmlFor="weight"><b>Weight</b></label>
-        <input type="text" placeholder="Enter Weight" name="weight" onChange={handleChange} value={weight}required/><br />
+      <label htmlFor="height"><b>Height</b></label>
+      <input type="text" placeholder="Enter Height" name="height" onChange={handleChange} value={height} required /><br />
 
-        
+      <label htmlFor="weight"><b>Weight</b></label>
+      <input type="text" placeholder="Enter Weight" name="weight" onChange={handleChange} value={weight} required /><br />
 
-        <label>
-          <input type="checkbox" defaultChecked= {true} name="remember" style={{marginBottom:"15px"}}/> Remember me
-        </label>
-        <p>By creating an account you agree to our <a href="#" style={{color:"dodgerblue"}}>Terms & Privacy</a>.</p>
-        <div className="clearfix">
-          <button type="button" className="cancelbtn" >Cancel</button>
-          <button type="submit" className="signupbtn" onClick={handleSubmit}>Sign Up</button>
-        </div>
+
+
+      <label>
+        <input type="checkbox" defaultChecked={true} name="remember" style={{ marginBottom: "15px" }} /> Remember me
+      </label>
+      <p>By creating an account you agree to our <a href="#" style={{ color: "dodgerblue" }}>Terms & Privacy</a>.</p>
+      <div className="clearfix">
+        <button type="button" className="cancelbtn" >Cancel</button>
+        <button type="submit" className="signupbtn" onClick={handleSubmit}>Sign Up</button>
       </div>
-      </>);
-    }
+    </div>
+  </>);
+}
--- nextjs-14/james-app/app/login/page.tsx
+++ nextjs-14/james-app/app/login/page.tsx
@@ -1,44 +1,59 @@
 'use client';
-import Image from "next/image";
+import { useRouter } from "next/navigation"
 import { useState } from "react";
 import axios from 'axios'
-import Link from "next/link";
+
 const SERVER = 'http://localhost:8080'
 export default function login() {
 
   const [username, setUsername] = useState('')
   const [password, SetPassword] = useState('')
 
-  const handleUsername = (e:any) => {setUsername(e.target.value)}
-  const handlePassword = (e:any) => {SetPassword(e.target.value)}
+  const handleUsername = (e: any) => { setUsername(e.target.value) }
+  const handlePassword = (e: any) => { SetPassword(e.target.value) }
 
-  
-  const handleSubmit = () => {alert("request가 가져가는 입력 ID" + username)
-  const url = `${SERVER}/api/login`
-  const data = {username , password} // <- (원본 un : un, pw : pw ) Key & value가 같으면 생략가능 -> username : username ~
-  const config = {
-    headers:{
-      "Cache-Control": "no-cache",
-      "Content-Type": "application/json",
-       Authorization: `Bearer blah ~` ,
-      "Access-Control-Allow-Origin": "*",
-    }   
+  const router = useRouter();
+
+  const handleSubmit = () => {
+    alert("request가 가져가는 입력 ID" + username)
+    const url = `${SERVER}/api/login`
+    const data = { username, password } // <- (원본 un : un, pw : pw ) Key & value가 같으면 생략가능 -> username : username ~
+    const config = {
+      headers: {
+        "Cache-Control": "no-cache",
+        "Content-Type": "application/json",
+        Authorization: `Bearer blah ~`,
+        "Access-Control-Allow-Origin": "*",
+      }
+    }
+
+    axios.post(url, data, config) // = requestbody
+      .then(res => {                       // res.data.message = 자바의 "message", 
+        const message = res.data.message   // JSON.stringify = toString() , res.data = responsebody
+        alert((message))
+        if (message === 'SUCCESS') {
+          router.push("/articles/new-article")
+        }
+        else if (message === 'FAIL') {
+          alert('FAIL');
+        }
+        else if (message === 'WRONG PASSWORD') {
+          alert('WRONG PASSWORD');
+        }
+        else {
+          alert('지정한 값 없음');
+        }
+      })
   }
-  axios.post(url,data,config) // = requestbody
-  .then(res=>{                            // res.data.messenger = 자바의 "messenger", 
-    const messenger = res.data.messenger   // JSON.stringify = toString() , res.data = responsebody
-    alert((messenger))
-  })
-}
 
 
-  
-  return(<>
-  <h1>로그인</h1>
-  <h3>ID</h3>
-  <input type = "text" onChange={handleUsername}/>
-  <h3>Password</h3>
-  <input type = "text" onChange={handlePassword}/> <br /> <br />
-  <button onClick={handleSubmit}>전송</button>
+
+  return (<>
+    <h1>로그인</h1>
+    <h3>ID</h3>
+    <input type="text" onChange={handleUsername} />
+    <h3>Password</h3>
+    <input type="text" onChange={handlePassword} /> <br /> <br />
+    <button onClick={handleSubmit}>전송</button>
   </>);
 }
